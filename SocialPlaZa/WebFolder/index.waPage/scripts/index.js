86f7ff2f2274384b9dda69108370f6cd63df4d03
Fran Caja <fcaja@socialerp.net>
2013-04-18 17:03:46 +0200

    index

---
+++ SocialPlaZa/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,21 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+	
+	fcBrain.welcome();
+	
+	
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
